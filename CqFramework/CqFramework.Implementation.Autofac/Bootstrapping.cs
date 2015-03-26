3691f11ebb695b3e6b0edf97804f0600b65f58bb
nengberg <niklas.engberg@creuna.se>
2015-03-26 15:45:47 +0100

    Adds bootstrapping of command dispatcher and query processor.

--- CqFramework/CqFramework.Implementation.Autofac/Bootstrapping.cs
+++ CqFramework/CqFramework.Implementation.Autofac/Bootstrapping.cs
@@ -5,6 +5,9 @@ using Autofac;
 namespace CqFramework.Implementation.Autofac {
 	public class Bootstrapping {
 		public static void Configure(ContainerBuilder containerBuilder) {
+			containerBuilder.RegisterType<QueryProcessor>().As<IQueryProcessor>();
+			containerBuilder.RegisterType<CommandDispatcher>().As<ICommandDispatcher>();
+
 			containerBuilder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
 				.Where(t => t.Name.EndsWith("QueryHandler"))
 				.AsImplementedInterfaces();
